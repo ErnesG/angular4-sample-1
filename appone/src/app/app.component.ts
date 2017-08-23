fe7e544f6908ccebf21f82cb755c8f13a3031d77
ErnesG <egonzl92@outlook.com>
2017-08-23 16:38:03 -0500

    Adds a little form with ngmodel

--- appone/src/app/app.component.ts
+++ appone/src/app/app.component.ts
@@ -6,11 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
   template:`<h1>{{title}}</h1><h2>{{hero.name}} details</h2>
   <div><label>id: {{hero.id}}</label></div>
-  <div><label>{{hero.name}}</label></div>`
+  <div><label>{{hero.name}}</label></div>
+  <div>
+   <label>name:</label>
+   <input [(ngModel)]="hero.name" placeholder = "name">
+  </div>`
 })
 export class AppComponent {
   title = 'Tour of heroes!';
-  hero: Hero{
+  hero: Hero={
     id: 1,
     name:'Windstorm'
   };
