cd759ecd1e0233dc676caa496a70e5f8852683fe
ErnesG <egonzl92@outlook.com>
2017-08-24 16:23:32 -0500

    settin up new component 1

--- appone/src/app/hero-detail/hero-detail.component.ts
+++ appone/src/app/hero-detail/hero-detail.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Hero} from './hero';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
-export class HeroDetailComponent implements OnInit {
+export class HeroDetailComponent{
 
-  constructor() { }
+ // constructor() { }
 
-  ngOnInit() {
-  }
+ 
+
 
 }
