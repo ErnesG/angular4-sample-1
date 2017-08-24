03b16dd60a317c7e7aa823ee5fcf13ced341c7e3
Ernesto Gon <egonzl92@outlook.com>
2017-08-23 23:31:02 -0500

    Making stuff right

--- appone/src/app/app.component.ts
+++ appone/src/app/app.component.ts
@@ -1,16 +1,9 @@
 import { Component } from '@angular/core';
-
+// implement external template for tomorrow
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  template:`<h1>{{title}}</h1><h2>{{hero.name}} details</h2>
-  <div><label>id: {{hero.id}}</label></div>
-  <div><label>{{hero.name}}</label></div>
-  <div>
-   <label>name:</label>
-   <input [(ngModel)]="hero.name" placeholder = "name">
-  </div>`
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Tour of heroes!';
@@ -18,7 +11,20 @@ export class AppComponent {
     id: 1,
     name:'Windstorm'
   };
+  heroes = HEROES;
 }
+const HEROES: Hero[] =[
+  {id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
 export class Hero {
   id: number;
   name: string;
